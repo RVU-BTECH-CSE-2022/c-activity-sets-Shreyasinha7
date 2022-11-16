39ec8a1888470e6f3d0b838b6ec451cba7f3b792
Shreya  Sinha <115694115+Shreyasinha7@users.noreply.github.com>
2022-11-16 05:16:04 +0000

    solved

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,16 +1,34 @@
 #include<stdio.h>
-main()
+int main()
 {
- char str1[10],str2[10];
-  print("enter the number");
-  scanf("%s%s,&str1,&str2");
-  int i;
-  for(i=0; str1[i];i++)
+  int a=0,b=0;
+  char str[10];
+  char var[10];
+  printf("Enter the 1st string:");
+  scanf("%s",&str);
+
+  printf("Enter the 2nd string:");
+  scanf("%s",&var);
+
+  printf("The length of the string str[] is :");
+  for (int i=0;str[i]!='\0';i++)
     {
-      for(i=0;str2[i];i++)
-        {
-          printf("/n");
-            }
-      }
-  printf("the compare=%d",i);
+      a++;
+    }
+    printf("%d\n",a);
+  
+  printf("The length of the string var[] is :");
+  for (int i=0;var[i]!='\0';i++)
+    {
+      b++;
+    }
+    printf("%d\n",b);
+  
+  if(a>b){
+    printf("length of str[10] is greater than var[10]:\n");
   }
+  else if(b>a)
+  {
+    printf("length of var[10] is greater than str[10]:\n");
+ }
+}
